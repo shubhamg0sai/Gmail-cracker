930d358b6d58d63186ad5cd0d992e9526b7e763d
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-01-25 10:03:27 +0530

    Update fastgmail.py

--- fastgmail.py
+++ fastgmail.py
@@ -1,29 +1,7 @@
-
-import os
-CorrectUsername = "g0sain"
-CorrectPassword = "sim"
-
-loop = 'true'
-while (loop == 'true'):
-    username = raw_input("\033[1;96m[#] \x1b[0;36m Enter Username\x1b[1;92m➤ ")
-    if (username == CorrectUsername):
-    	password = raw_input("\033[1;96m[#] \x1b[0;36m Enter Password\x1b[1;92m➤ ")
-        if (password == CorrectPassword):
-            print "Logged in successfully as " + username #fb-cloning-id SG
-            loop = 'false'
-        else:
-            print "Wrong password!"
-            os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
-    else:
-        print "Wrong username!"
-        os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
-
 import smtplib
-
 smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
-
 
 print('''
 ==========================================================
